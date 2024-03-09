c5fa7886b99271b2a15308fef8d6d619622decc0
Sumanth Perambuduri <35676632+supersu-man@users.noreply.github.com>
2024-03-09 13:19:35 +0530

    better colors and UI

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
-let eventTemplate = (index, heading, date, description, images) => {
+loadData()
 
-    let imageList = `
-    <div class="col-12 p-1 fs-4">${heading}</div>
-    <div class="col-12 p-1">${date}</div>
-    <div class="accordion accordion-flush g-0 p-1 ${description ? '' : 'visually-hidden'}">
-        <div class="accordion-item bg-black">
-            <div class="accordion-header">
-                <div type="button" class="accordion-button collapsed px-1 py-2 bg-dark text-white" data-bs-toggle="collapse" data-bs-target="#collapse${index}">Description</div>
-            </div>
-            <div id="collapse${index}" class="accordion-collapse collapse text-white" >
-                <div class="accordion-body p-1">${description}</div>
-            </div>
-        </div>
-    </div>
-    `
-    images.forEach(image => {
-        imageList+=`
-            <div class="col-auto p-1">
-                <img class="img-fluid" src="assets/images/${image}" />
-            </div>
-        `
-    });
-
-    return `
-        <div class="row pt-4">
-            ` + 
-            imageList
-            +
-            `
-        </div>
-    `
-}
-
-this.loadJSON((response) => {
-    let data = JSON.parse(response)
-    let eventList = ''
-    data.forEach((event, index) => {
-        eventList+=eventTemplate(index, event.heading, event.date, event.description, event.images)
-    })
-    this.document.body.innerHTML = eventList
-})
-
-function loadJSON(callback) {
+async function loadData(){
     var xObj = new XMLHttpRequest();
     xObj.overrideMimeType("application/json");
     xObj.open('GET', './cycling.json', true);
     xObj.onreadystatechange = () => {
-        if (xObj.readyState === 4 && xObj.status === 200) callback(xObj.responseText); 
+        if (xObj.readyState === 4 && xObj.status === 200) {
+            //xObj.responseText
+            let data = JSON.parse(xObj.responseText)
+            let eventList = ''
+            data.forEach((event, index) => {
+                eventList+=eventTemplate(index, event.heading, event.date, event.description, event.images)
+            })
+            document.body.innerHTML += eventList
+            let script = document.createElement('script')
+            script.id = "ID";
+            script.src = "https://cdn.jsdelivr.net/npm/nanogallery2@3/dist/jquery.nanogallery2.min.js";
+            script.type = "text/javascript";
+            document.getElementsByTagName('head')[0].appendChild(script);
+        }
     };
     xObj.send(null);
+}
+
+let eventTemplate = (index, heading, date, description, images) => {
+    let eventImages = ''
+    images.forEach(image => {
+        eventImages+=`
+            <a href="${image}" data-ngThumb="${image}"></a>
+        `
+    });
+    return `
+        <div class="fs-4 pt-2">${heading}</div>
+        <div>${date}</div>
+        <div class="accordion accordion-flush my-2 ${description ? '' : 'visually-hidden'}">
+            <div class="accordion-item">
+                <div class="accordion-header">
+                    <div type="button" class="accordion-button collapsed text-white" data-bs-toggle="collapse" data-bs-target="#collapse${index}">Description</div>
+                </div>
+                <div id="collapse${index}" class="accordion-collapse collapse text-white">
+                    <div class="accordion-body py-2 px-3">${description}</div>
+                </div>
+            </div>
+        </div>
+        <div id="nanogallery2" class="pb-2" data-nanogallery2='{ "thumbnailHeight": 200, "thumbnailWidth": "auto", "itemsBaseURL": "/assets/images/", "thumbnailBorderHorizontal": 0, "thumbnailBorderVertical": 0}'>
+            ${eventImages}
+        </div>
+    `
 }
